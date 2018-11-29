37810c77d75fb3918820ba87cbf1eb0f916fd59f
filip <filip.christofferson@gmail.com>
2018-11-29 12:32:56 +0100

    Add Entities package and Entity class, add some variables and a method to Entity.java

---
+++ src/org/ia/entities/Entity.java
@@ -0,0 +1,13 @@
+package org.ia.entities;
+
+import java.util.List;
+
+public class Entity {
+    private int hp;
+    List moveList = new List<Action>();
+}
+
+    public void makeAction(Action){
+    return null;
+    }
+}
